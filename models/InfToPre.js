4478165434dd292f8e9db3af934ea4ea4b32a881
Jhonatan32234 <152237788+Jhonatan32234@users.noreply.github.com>
2024-04-21 21:20:14 -0600

    Update InfToPre.js

--- models/InfToPre.js
+++ models/InfToPre.js
@@ -1,52 +1,58 @@
 
-let results;
+import { LinkedList } from "./LinkedList.js";
+
 export class InfToPre{
-
-
-getData(){
-    console.log(results);
-    document.getElementById("text").innerHTML+=results+"\n";
-}
-
-infijaAprefija(dato) {
    
-    let prefija = '';
-    let arreglo = [];
-    
-    for(let i = 0; i < dato.length; i++) {
-        let entrada = dato[i];
 
-        if(/[a-zA-Z0-9]/.test(entrada)) {
-            prefija += entrada;
-        } else if(entrada === '(') {
-            arreglo.push(entrada);
-        } else if(entrada === ')') {
-            while(arreglo.length > 0 && arreglo[arreglo.length-1] !== '(') {
-                prefija += arreglo.pop();
+ infixToPrefix(infix) {
+    let precedence = {
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+        '^': 3
+    };
+
+    let operators = new LinkedList();
+    let operands = new LinkedList();
+
+    for (let i = infix.length - 1; i >= 0; i--) {
+        let char = infix[i];
+        if (char === ')' || char === '(') continue;
+        else if (char in precedence) {
+            while (operators.head !== null && precedence[operators.head.data] >= precedence[char]) {
+                let op = operators.head.data;
+                operators.head = operators.head.next;
+
+                let operand1 = operands.head.data;
+                operands.head = operands.head.next;
+
+                let operand2 = operands.head.data;
+                operands.head = operands.head.next;
+
+                operands.add(op + operand2 + operand1);
             }
-            arreglo.pop();
+
+            operators.add(char);
         } else {
-            while(arreglo.length > 0 && this.operador(entrada) <= this.operador(arreglo[arreglo.length-1])) {
-                prefija += arreglo.pop();
-            }
-            arreglo.push(entrada);
+            operands.add(char);
         }
     }
-    
-    while(arreglo.length > 0) {
-        prefija += arreglo.pop();
+
+    while (operators.head !== null) {
+        const op = operators.head.data;
+        operators.head = operators.head.next;
+
+        const operand1 = operands.head.data;
+        operands.head = operands.head.next;
+
+        const operand2 = operands.head.data;
+        operands.head = operands.head.next;
+
+        operands.add(op + operand2 + operand1);
     }
-    results=prefija;
-}
-operador(operador) {
-    switch(operador) {
-        case '+':
-        case '-': return 1;
-        case '*':
-        case '/': return 2;
-        case '^': return 3;
-        default: return 0;
-    }
+
+    return operands.display();
 }
 
 }
