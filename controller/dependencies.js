82be379af549c5d17e6ffa0e511b76a6af3bf83a
Jhonatan32234 <152237788+Jhonatan32234@users.noreply.github.com>
2024-04-21 21:29:38 -0600

    Update dependencies.js

--- controller/dependencies.js
+++ controller/dependencies.js
@@ -1,5 +1,6 @@
 import { InfToPre } from "../models/InfToPre.js";
-
+import { LinkedList } from "../models/LinkedList.js";
 let inTopre = new InfToPre();
-
+let linkedList = new LinkedList();
 export {inTopre};
+export{linkedList};
